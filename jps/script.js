b9be84330f4653bba00379823d208720f8a66b32
jerops <jeropsweb@gmail.com>
2022-01-24 14:03:09 +0100

    last commit

--- jps/script.js
+++ jps/script.js
@@ -8,36 +8,50 @@ const progress = document.getElementById('progress');
 //play and pause video
 
 function toggleVideoStatus() {
-  if (video.pauded) {
+  if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
 }
-//update play icon
+//update play/pause icon
 
 function updatePlayIcon() {
-  if (video.pauded) {
-    play.innerHTML = '<i class=fa fa-play fa-2x></i>';
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
-    play.innerHTML = '<i class=fa fa-pause fa-2x></i>';
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
 
 //update progres and timestamp
 
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //get minutes
+
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+  //get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-//set video tiem to progess bar
+//set manualy video tiem to progess bar
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-//Stop viedo
+//Stop video
 function stopVideo() {
-  return true;
+  video.currentTime = 0;
+  video.pause();
 }
 
 //event listeners
@@ -45,7 +59,7 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeupdade', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
